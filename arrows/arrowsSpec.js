3295b26dd3e59de52eb3498a25aa60b33f01fcc1
James Simoes <james_simoes@optum.com>
2015-02-04 20:44:24 -0500

    getting things ready

---
+++ arrows/arrowsSpec.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jsimoes on 2/4/2015.
+ */
+
+'use strict';
+
+describe('ES6: Arrows',function(){
+    it('should be loaded',function(){
+        expect(true).toBe(true);
+    });
+});
---
+++ classes/classesSpec.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jsimoes on 2/4/2015.
+ */
+'use strict';
+
+describe('ES6: Classes',function(){
+    it('should be loaded',function(){
+        expect(true).toBe(true);
+    });
+});
